5de3459ef0ddf3475a92d7548d9e62928dbbe983
RaymondLoranger <Raymond.Loranger@bell.net>
2019-01-03 12:41:41 -0500

    0.1.0

--- lib/gen_server_proxy/behaviour.ex
+++
@@ -1,4 +0,0 @@
-defmodule GenServer.Proxy.Behaviour do
-  @callback server_name(server_id :: term) :: GenServer.name()
-  @callback server_unregistered(server_id :: term) :: term
-end
--- mix.exs
+++ mix.exs
@@ -7,7 +7,30 @@ defmodule GenServer.Proxy.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      name: "GenServer Proxy",
+      source_url: source_url(),
+      description: description(),
+      package: package(),
       deps: deps()
+    ]
+  end
+
+  defp source_url do
+    "https://github.com/RaymondLoranger/gen_server_proxy"
+  end
+
+  defp description do
+    """
+    Performs a GenServer call, cast or stop. Will wait a bit if the server is not yet registered on restarts.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "config/persist*.exs"],
+      maintainers: ["Raymond Loranger"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => source_url()}
     ]
   end
 
@@ -23,7 +46,7 @@ defmodule GenServer.Proxy.MixProject do
     [
       {:mix_tasks,
        github: "RaymondLoranger/mix_tasks", only: :dev, runtime: false},
-      {:file_only_logger, github: "RaymondLoranger/file_only_logger"},
+      {:file_only_logger, "~> 0.1"},
       {:persist_config, "~> 0.1"},
       {:earmark, "~> 1.0", only: :dev},
       {:ex_doc, "~> 0.14", only: :dev, runtime: false},
