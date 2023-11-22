63e0d64878874bb024bb3bcdaf8fb87257cbdd2f
RaymondLoranger <Raymond.Loranger@bell.net>
2023-11-21 21:31:23 -0500

    0.1.49

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,14 +1,8 @@
-env = Application.get_env(:file_only_logger, :logger)
+env = Application.get_env(:file_only_logger, :logger, [])
 
-# Delete log files before test...
+# Truncate log files before test...
 for {:handler, _handler_id, :logger_std_h, %{config: %{file: path}}} <- env,
-    do: File.rm(path)
-
-# Maybe wait to ensure reading file information prior to writing.
-case for {:handler, _, _, %{config: %{file_check: file_check_in_ms}}} <- env,
-         do: file_check_in_ms do
-  [] -> :ok
-  file_checks_in_ms -> :ok = Enum.max(file_checks_in_ms) |> Process.sleep()
-end
+    # `File.rm/1` would prevent test logging when file_check > 0.
+    do: File.open(path, [:write])
 
 ExUnit.start()
