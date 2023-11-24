7ecdcf7c5b8f749da61bdfe9ca2aa381893ad8dd
RaymondLoranger <Raymond.Loranger@bell.net>
2023-11-24 13:21:29 -0500

    0.1.51

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,4 @@
-env = Application.get_env(:file_only_logger, :logger, [])
+env = :application.get_env(:file_only_logger, :logger, [])
 
 # Truncate log files before test...
 for {:handler, _handler_id, :logger_std_h, %{config: %{file: path}}} <- env,
