3166e28441c234ca2ecae88561f7531f1b8a1e24
RaymondLoranger <Raymond.Loranger@bell.net>
2024-02-24 20:24:07 -0500

    0.1.54

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GenServer.Proxy.MixProject do
   def project do
     [
       app: :gen_server_proxy,
-      version: "0.1.53",
+      version: "0.1.54",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "GenServer Proxy",
