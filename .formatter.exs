36b635eba786d5af1a1c17755ee0b3409087cfbb
RaymondLoranger <Raymond.Loranger@bell.net>
2026-05-29 14:35:47 -0400

    "0.1.71"

--- .formatter.exs
+++ .formatter.exs
@@ -1,10 +1,5 @@
 # Used by "mix format"
-wildcard = fn glob -> Path.wildcard(glob, match_dot: true) end
-matches = fn globs -> Enum.flat_map(globs, &wildcard.(&1)) end
-except = []
-inputs = ["*.exs", "{config,lib,test}/**/*.{ex,exs}"]
-
 [
-  inputs: matches.(inputs) -- matches.(except),
+  inputs: ["*.exs", "{config,lib,test}/**/*.{ex,exs}"],
   line_length: 80
 ]
